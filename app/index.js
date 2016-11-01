6406ab33ddbe821a357f6ee286f1fc21a59ba695
dong.keke <dong.keke@tongbanjie.com>
2016-11-01 18:27:37 +0800

    完善目录配置

--- app/index.js
+++ app/index.js
@@ -2,13 +2,19 @@ var generators = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var path = require('path');
+var fs = require('fs');
 
 var Util = {
   templates : [
+    "bower.json",
     "package.json",
     "gulpfile.js",
     ".gitignore",
-    "src/index.html"
+    "src/index.html",
+    "src/common/base.styl",
+    "src/common/base.js",
+    "src/page/index/index.js",
+    "src/page/index/index.styl"
   ],
   getPrompts:function(_this){
     var prompts = [{
@@ -48,6 +54,16 @@ var Util = {
     for(var i=0;i<arr.length;i++){
       _this.copy(arr[i],arr[i]);
     }
+  },
+  resetConfig:function(filePath,_this){
+    var packagePath = path.join(__dirname,filePath);
+    var info = JSON.parse(fs.readFileSync(packagePath));
+
+    info.name = _this.name;
+    info.description = _this.description;
+    info.author = _this.author;
+
+    fs.writeFileSync(packagePath,JSON.stringify(info,null,2));
   }
 };
 
@@ -73,6 +89,17 @@ module.exports = generators.Base.extend({
       this.author = props.author;
       this.description = props.description;
 
+      // var packagePath = path.join(__dirname,'templates/package.json');
+      // var info = JSON.parse(fs.readFileSync(packagePath));
+
+      // info.name = this.name;
+      // info.description = this.description;
+      // info.author = this.author;
+
+      // fs.writeFileSync(packagePath,JSON.stringify(info,null,2));
+      Util.resetConfig('templates/package.json',this);
+      Util.resetConfig('templates/bower.json',this);
+
       done();  //进入下一个生命周期阶段
     }.bind(this));
   },
@@ -85,6 +112,15 @@ module.exports = generators.Base.extend({
   install:function (){
     var done = this.async();
     this.spawnCommand('npm',['install']).on('exit',function(code) {
+      if(code){
+        done(new Error('code:'+code));
+        // console.log(code)
+      }else{
+        done();
+      }
+    }).on('error', done);
+
+    this.spawnCommand('bower',['install']).on('exit',function(code) {
       if(code){
         done(new Error('code:'+code));
         // console.log(code)
--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,6 +3,12 @@ var fs           = require('fs');
 var del          = require('del');
 var connect      = require('gulp-connect');
 
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+var uglify = require('gulp-uglify');
+
+var bower = require('gulp-bower');
+
 var jsonObj = JSON.parse(fs.readFileSync('./package.json'));
 
 //基础变量
@@ -26,9 +32,29 @@ gulp.task('clean',function(cb) {
   del(['build'], cb);
 });
 
-gulp.task('copy',function(){
-  return gulp.src('./src/**/*.*')
-    .pipe(gulp.dest(paths.build));
+gulp.task('copy',['stylCompile','uglifyJs'],function(){
+  return gulp.src(paths.src + '/**/*.html')
+    .pipe(gulp.dest(paths.build+'/'));
+});
+
+gulp.task('stylCompile', function(){
+  return gulp.src(paths.src + '/**/*.styl')
+    .pipe(stylus({
+      use : nib(),
+      compress : true
+    }))
+    .pipe(gulp.dest(paths.build +'/'))
+});
+
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest(paths.build +'/common/lib'))
+});
+
+gulp.task("uglifyJs",function(){
+  return gulp.src(paths.src + "/**/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.build +'/'))
 });
 
 gulp.task('default', ['clean','connect']);
